43ea44b183907508f6253fdfde293f74ccb7a825
wrawka <wrawka@gmail.com>
2020-07-21 17:27:10 +0300

    Created

---
+++ 07_templateTABLE/table.cpp
@@ -0,0 +1,26 @@
+#include "D:\\MD\\Lebedev\\projects\\YandexRed\\TestRunner\\test_runner.h"
+
+using namespace std;
+
+// Реализуйте здесь шаблонный класс Table
+template <typename T> 
+class Table {
+
+};
+
+void TestTable() {
+  Table<int> t(1, 1);
+  ASSERT_EQUAL(t.Size().first, 1u);
+  ASSERT_EQUAL(t.Size().second, 1u);
+  t[0][0] = 42;
+  ASSERT_EQUAL(t[0][0], 42);
+  t.Resize(3, 4);
+  ASSERT_EQUAL(t.Size().first, 3u);
+  ASSERT_EQUAL(t.Size().second, 4u);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestTable);
+  return 0;
+}
