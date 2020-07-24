e9a53f3337affcefbf2af0d45ba2c66c98427b0c
wrawka <wrawka@gmail.com>
2020-07-24 15:15:32 +0300

    [MOSTLY WORKING] deque

--- 08_classDeque/deque.cpp
+++ 08_classDeque/deque.cpp
@@ -1,87 +1,33 @@
 #include "D:\\MD\\Lebedev\\projects\\YandexRed\\TestRunner\\test_runner.h"
-
-#include <stdexcept>
-#include <vector>
+#include "deque.h"
 
 using namespace std;
-
-template <typename T>
-class Deque
-{
-private:
-    vector<T> forward;
-    vector<T> backward;
-    size_t size;
-public:
-    // конструктор по умолчанию;
-    Deque() {
-        forward.clear();
-        backward.clear();
-        size = 0;
-    }
-    // константный метод Empty, возвращающий true, если дек не содержит ни одного элемента;
-    bool Empty() const {
-        return size == 0;
-    }
-    // константный метод Size, возвращающий количество элементов в деке;
-    size_t Size() const {
-        return size;
-    }
-    T& operator[](size_t index) {
-        size_t n = backward.size();
-        if (index <= n) {
-            return backward[n - index];
-        }
-        return forward[index - n];
-    }
-    const T& operator[](size_t index) const {
-        size_t n = backward.size();
-        if (index <= n) {
-            return backward[n - index];
-        }
-        return forward[index - n];
-    }
-    // метод At(size_t index), генерирующий стандартное исключение out_of_range, если индекс больше или равен количеству элементов в деке;
-    T& At(size_t index) {
-        if (0 > index || index > size) throw out_of_range("Index out of bounds");
-        return this->operator[](index);
-    }
-    const T& At(size_t index) const;
-    T& Front() {
-        return backward.front();
-    }
-    const T& Front() const {
-        return backward.back();
-    }
-    T& Back() {
-        return forward.back();
-    }
-    const T& Back() const {
-        return forward.back();
-    }
-    void PushFront(const T& new_value) {
-        backward.push_back(new_value);
-        size++;
-    }
-    void PushBack(const T& new_value) {
-        forward.push_back(new_value);
-        size++;
-    }
-
-};
 
 void TestDeque() {
     Deque<int> d;
     ASSERT(d.Empty());
     d.PushBack(100);    //{100}
+    ASSERT_EQUAL(d.Front(), 100);
     d.PushFront(0);     //{0, 100}
-    ASSERT_EQUAL(d.Size(), 2u);
-    ASSERT_EQUAL(d[0], 0u);
     d.PushBack(101);    //{0, 100, 101}
+    ASSERT_EQUAL(d.Size(), 3u);
+    ASSERT_EQUAL(d[0], 0u);
+    ASSERT_EQUAL(d[1], 100u);
+    ASSERT_EQUAL(d[2], 101u);
     d.PushFront(-1);    //{-1, 0, 100, 101}
     ASSERT_EQUAL(d.Front(), -1);
-    ASSERT_EQUAL(d.Back(), 101u);
-    ASSERT_EQUAL(d.At(2), 100u);
+    d.PushBack(300);    //{-1, 0, 100, 101, 300}
+    ASSERT_EQUAL(d.Back(), 300u);
+    d.PushFront(-10);   //{-10, -1, 0, 100, 101, 300}
+    d.Dubug(cout);
+    ASSERT_EQUAL(d.Front(), -10);
+    ASSERT_EQUAL(d.Back(), 300u);
+    ASSERT_EQUAL(d.At(0), -10);
+    ASSERT_EQUAL(d.At(1), -1);
+    ASSERT_EQUAL(d.At(2), 0u);
+    ASSERT_EQUAL(d.At(3), 100u);
+    d[2] = -2;
+    ASSERT_EQUAL(d.At(2), -2);
     try
     {
         cout << d.At(13);
@@ -90,8 +36,6 @@ void TestDeque() {
     {
         std::cerr << e.what() << '\n';
     }
-    
-
 }
 
 int main() {
---
+++ 08_classDeque/deque.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+template <typename T>
+class Deque
+{
+private:
+    vector<T> forward;
+    vector<T> backward;
+    size_t size;
+
+public:
+    // конструктор по умолчанию;
+    Deque() {
+        forward.clear();
+        backward.clear();
+        size = 0;
+    }
+    // константный метод Empty, возвращающий true, если дек не содержит ни одного элемента;
+    bool Empty() const {
+        return size == 0;
+    }
+    // константный метод Size, возвращающий количество элементов в деке;
+    size_t Size() const {
+        return size;
+    }
+    T& operator[](size_t index) {
+        if (index == 0) return backward.back();
+        int n = backward.size();
+        if (index < n) {
+            auto it = backward.rbegin();
+            advance(it, index);
+            return *it;
+        } else {
+            auto it = forward.begin();
+            advance(it, index - n);
+            return *it;
+        }
+    }
+    const T& operator[](size_t index) const {
+        if (index == 0) return backward.back();
+        int n = backward.size();
+        if (index < n) {
+            auto it = backward.rbegin();
+            advance(it, index);
+            return *it;
+        } else {
+            auto it = forward.begin();
+            advance(it, index - n);
+            return *it;
+        }
+    }
+    // метод At(size_t index), генерирующий стандартное исключение out_of_range, если индекс больше или равен количеству элементов в деке;
+    T& At(size_t index) {
+        if (0 > index || index > size) throw out_of_range("Index out of bounds");
+        return this->operator[](index);
+    }
+    const T& At(size_t index) const {
+        if (0 > index || index > size) throw out_of_range("Index out of bounds");
+        return this->operator[](index);
+    }
+    T& Front() {
+        if (backward.empty()) {
+            return forward.front();
+        }
+        else {
+            return backward.back();
+        }
+    }
+    const T& Front() const {
+        if (backward.empty()) {
+            return forward.front();
+        }
+        else {
+            return backward.back();
+        }
+    }
+    T& Back() {
+        if (forward.empty()) {
+            return backward.front();
+        }
+        else {
+            return forward.back();
+        }
+    }
+    const T& Back() const {
+        if (forward.empty()) {
+            return backward.front();
+        }
+        else {
+            return forward.back();
+        }
+    }
+    void PushFront(const T& new_value) {
+        backward.push_back(new_value);
+        size++;
+    }
+    void PushBack(const T& new_value) {
+        forward.push_back(new_value);
+        size++;
+    }
+    ostream& Dubug(ostream& os) {
+        os << "Forward vector: ";
+        for (auto i : forward) {
+            os << i << " ";
+        }
+        os << endl;
+        os << "Backward vector: ";
+        for (auto i : backward) {
+            os << i << " ";
+        }
+        return os << endl;
+    }
+};
