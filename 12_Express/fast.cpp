f2ccd2a481cdbeb5372951b1542887bcca857708
wrawka <wrawka@gmail.com>
2020-07-29 17:50:36 +0300

    ---

--- 12_Express/fast.cpp
+++ 12_Express/fast.cpp
@@ -36,29 +36,52 @@ private:
   map<int, set<int>> reachable_lists_;
 };
 
-void TestRoutes() {
-
+void TestRoutes1() {
+  // Test #1
+  RouteManager test;
+  test.AddRoute(-2, 5);
+  test.AddRoute(10, 4);
+  test.AddRoute(5, 8);
+  ASSERT_EQUAL(test.FindNearestFinish(4, 10), 0);
+  ASSERT_EQUAL(test.FindNearestFinish(4, -2), 6);
+  ASSERT_EQUAL(test.FindNearestFinish(5, 0), 2);
+  ASSERT_EQUAL(test.FindNearestFinish(5, 100), 92);
 }
 
+void TestRoutes() {
+  // Custom
+  RouteManager test;
+  // TODO: наполнение!
+  test.AddRoute(-2, 5);
+  test.AddRoute(10, 4);
+  test.AddRoute(5, 8);
+  ASSERT_EQUAL(test.FindNearestFinish(4, 10), 0);
+  ASSERT_EQUAL(test.FindNearestFinish(4, -2), 6);
+  ASSERT_EQUAL(test.FindNearestFinish(5, 0), 2);
+  ASSERT_EQUAL(test.FindNearestFinish(5, 100), 92);
+}
 
 int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestRoutes1);
 
-  RouteManager routes;
 
-  int query_count;
-  cin >> query_count;
+  // RouteManager routes;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-    int start, finish;
-    cin >> start >> finish;
-    if (query_type == "ADD") {
-      routes.AddRoute(start, finish);
-    } else if (query_type == "GO") {
-      cout << routes.FindNearestFinish(start, finish) << "\n";
-    }
-  }
+  // int query_count;
+  // cin >> query_count;
+
+  // for (int query_id = 0; query_id < query_count; ++query_id) {
+  //   string query_type;
+  //   cin >> query_type;
+  //   int start, finish;
+  //   cin >> start >> finish;
+  //   if (query_type == "ADD") {
+  //     routes.AddRoute(start, finish);
+  //   } else if (query_type == "GO") {
+  //     cout << routes.FindNearestFinish(start, finish) << "\n";
+  //   }
+  // }
 
   return 0;
 }
