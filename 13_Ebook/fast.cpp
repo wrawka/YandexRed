57c865887f319dd0eb83e6107cd7f2f96c310b8e
wrawka <wrawka@gmail.com>
2020-08-05 12:21:16 +0300

    Clean slate

--- 13_Ebook/fast.cpp
+++ 13_Ebook/fast.cpp
@@ -2,56 +2,65 @@
 #include <iostream>
 #include <vector>
 #include <utility>
-#include <set>
-#include <algorithm>
 
 #include "test_runner.h"
 
 using namespace std;
 
-struct User {
-  // User(int f, int s) : user_id(f), progress(s) {}
-  int user_id;
-  int progress;
-};
-
-using myIt = set<User>::iterator;
-
-// auto id_ordering = [](User A, User B) { return A.user_id < B.user_id; };
-// bool id_ordering (User a, User b) { return a.user_id < b.user_id; }
-bool p_ordering (User a, User b) { return a.progress < b.progress; };
+template <typename T>
+void PrintVector(vector<T> source, string hint) {
+  cout << "Printing " << hint << ": ";
+  for (auto out : source) {
+    cout << out << " ";
+  }
+  cout << endl;
+}
 
 class ReadingManager {
 public:
   ReadingManager()
-      : access(MAX_USER_COUNT_ + 1),
-        users() {}
+      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
+        sorted_users_(),
+        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
 
-/* READ user page — сохранить факт того, что пользователь под номером user дочитал книгу до страницы page.
-Если ранее такой пользователь не встречался, необходимо его добавить. 
-Гарантируется, что в рамках одного пользователя номера страниц в соответствующих ему событиях возрастают. */
   void Read(int user_id, int page_count) {
-    if (AddUser(user_id, page_count)) return;
-    UpdateUser(user_id, page_count);
+    if (user_page_counts_[user_id] == 0) {
+      AddUser(user_id);
+    }
+    user_page_counts_[user_id] = page_count;
+    int& position = user_positions_[user_id];
+    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
+      SwapUsers(position, position - 1);
+    }
   }
 
-/* CHEER user — сообщить пользователю user, какая доля существующих пользователей (не считая его самого)
-прочитала меньшую часть книги, чем он. Если этот пользователь на данный момент единственный, 
-доля считается равной 1. Если для данного пользователя пока не было ни одного события READ, 
-доля считается равной 0, а сам пользователь не учитывается при вычислении долей 
-для других пользователей до тех пор, пока для него не случится событие READ. */
   double Cheer(int user_id) const {
-    const User& user = *access[user_id];
-    if (user.progress == 0) return 0;
-    if (users.size() ==1 ) return 1;
-    User zero{0, 0};
-    auto l_position = users.upper_bound(zero);
-    auto h_position = users.lower_bound(user);
-    double diff = distance(l_position, h_position) / distance(l_position, users.end());
+    if (user_page_counts_[user_id] == 0) {
+      return 0;
+    }
+    const int user_count = GetUserCount();
+    if (user_count == 1) {
+      return 1;
+    }
+    const int page_count = user_page_counts_[user_id];
+    int position = user_positions_[user_id];
+    while (position < user_count &&
+      user_page_counts_[sorted_users_[position]] == page_count) {
+      ++position;
+    }
+    if (position == user_count) {
+        return 0;
+    }
     // По умолчанию деление целочисленное, поэтому
     // нужно привести числитель к типу double.
     // Простой способ сделать это — умножить его на 1.0.
-    return diff;
+    return (user_count - position) * 1.0 / (user_count - 1);
+  }
+
+  void Dump() const {
+    PrintVector(user_page_counts_, "user page count");
+    PrintVector(sorted_users_, "sorted users");
+    PrintVector(user_positions_, "user positions");
   }
 
 private:
@@ -60,41 +69,29 @@ private:
   // в данном случае константная.
   // Будь она публичной, к ней можно было бы обратиться снаружи
   // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_USER_COUNT_ = 10;
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
+  vector<int> user_page_counts_;  // индекс вектора [user_id] -> кол-во страниц
+  vector<int> sorted_users_;   // user_id внутри; отсортированы по убыванию количества страниц
   vector<int> user_positions_; // позиции в векторе sorted_users_
 
-// TODO: not working
-  set<User, decltype(p_ordering)*> users; // список пользователей, упорядоченный по прогрессу
-  vector<myIt> access;  // доступ на список выше
-
-
-  int GetUserCount() const {
-    return users.size();
+  int GetUserCount() const {    // O(1)
+    return sorted_users_.size(); 
   }
-
-  bool AddUser(int new_id, int p_count = 0) {
-    auto add = users.insert({new_id, p_count});
-    if (add.second) { access[new_id] = add.first; };
-    return add.second;
+  
+  void AddUser(int user_id) {   // O(1)
+    sorted_users_.push_back(user_id); 
+    user_positions_[user_id] = sorted_users_.size() - 1;
   }
-
-  void UpdateUser(int user_id, int p_count) {
-    auto user = access[user_id];
-    users.erase(user);
-    AddUser(user_id, p_count);
-  }
-
 
   void SwapUsers(int lhs_position, int rhs_position) {
     const int lhs_id = sorted_users_[lhs_position];
     const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
+    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]); // O(1)
+    swap(user_positions_[lhs_id], user_positions_[rhs_id]);         // O(1)
   }
 };
+
 
 void TestReadingManager() {
   // Coursera Test #1
@@ -111,9 +108,8 @@ void TestReadingManager() {
   rm.Read(3, 11);
   ASSERT_EQUAL(rm.Cheer(3), 1);
   ASSERT_EQUAL(rm.Cheer(1), 0.5);
-
+  rm.Dump();
 }
-
 
 int main() {
   // Для ускорения чтения данных отключается синхронизация
@@ -121,29 +117,31 @@ int main() {
   // а также выполняется отвязка cin от cout
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  // TODO: ^^^ проверь, что тут происходит ^^^
 
   TestRunner tr;
+
   RUN_TEST(tr, TestReadingManager);
 
-  ReadingManager manager;
+  // ReadingManager manager;
 
-  int query_count;
-  cin >> query_count;
+  // int query_count;
+  // cin >> query_count;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-    int user_id;
-    cin >> user_id;
+  // for (int query_id = 0; query_id < query_count; ++query_id) {
+  //   string query_type;
+  //   cin >> query_type;
+  //   int user_id;
+  //   cin >> user_id;
 
-    if (query_type == "READ") {
-      int page_count;
-      cin >> page_count;
-      manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
-    }
-  }
+  //   if (query_type == "READ") {
+  //     int page_count;
+  //     cin >> page_count;
+  //     manager.Read(user_id, page_count);
+  //   } else if (query_type == "CHEER") {
+  //     cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+  //   }
+  // }
 
   return 0;
 }
