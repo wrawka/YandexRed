e465cc71357de8eb1f6d8a8db277e7f4820ecaad
wrawka <wrawka@gmail.com>
2020-08-06 11:31:33 +0300

    Reader is pair<int, int> now

--- 13_Ebook/fast.cpp
+++ 13_Ebook/fast.cpp
@@ -5,40 +5,26 @@
 #include <algorithm>
 #include <set>
 
-// #include "test_runner.h"
+#include "test_runner.h"
 
 using namespace std;
 
-// template <typename T>
-// void PrintSomething(T source, string hint) {
-//   cout << "Printing " << hint << ": ";
-//   for (auto out : source) {
-//     cout << out << " ";
-//   }
-//   cout << endl;
-// }
+template <typename T>
+void PrintSomething(T source, string hint) {
+  cout << "Printing " << hint << ": \n";
+  for (auto out : source) {
+    cout << out << " ";
+  }
+  cout << endl;
+}
 
 
-struct Reader
-{
-  int user_id;
-  int pages;
-};
+using Reader = pair<int, int>; //first is pages count, second is user_id
 
 using myIt = set<Reader>::iterator;
 
-// auto SortByPages = [](Reader lhs, Reader rhs) { return lhs.pages < rhs.pages; };
-
-// bool SortByPages(Reader lhs, Reader rhs) {
-//   return lhs.pages < rhs.pages;
-// }
-
-bool operator<(Reader lhs, Reader rhs) {
-  return (lhs.pages <= rhs.pages) && (lhs.user_id != rhs.user_id);
-}
-
-ostream& operator<<(ostream& os, Reader& r) {
-  return os << "ID:" << r.user_id << " finished " << r.pages << " pages\n";
+ostream& operator<<(ostream& os, Reader r) {
+  return os << r.second << ":" << r.first;
 }
 
 class ReadingManager {
@@ -63,18 +49,20 @@ public:
     if (user_count == 1) {
       return 1;
     }
-    auto losers = distance(readers.begin(), reader_access[user_id]);
-    // cout << "'distance' for UID:" << user_id << " is " << losers << endl;
+    auto page_count = reader_access[user_id]->first;
+    Reader u{page_count, 0};
+    // auto losers = distance(readers.begin(), reader_access[user_id]);
+    auto losers = distance(readers.begin(), readers.lower_bound(u));
     // По умолчанию деление целочисленное, поэтому
     // нужно привести числитель к типу double.
     // Простой способ сделать это — умножить его на 1.0.
     return (losers) * 1.0 / (user_count - 1);
   }
 
-  // void Dump() const {
-  //   PrintSomething(readers, "readers details");
-  //   PrintSomething(registered_users, "registered readers");
-  // }
+  void Dump() const {
+    PrintSomething(readers, "readers details");
+    PrintSomething(registered_users, "registered readers");
+  }
 
 private:
   // Статическое поле не принадлежит какому-то конкретному
@@ -82,26 +70,21 @@ private:
   // в данном случае константная.
   // Будь она публичной, к ней можно было бы обратиться снаружи
   // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_USER_COUNT_ = 10; //0'000;
 
-  // vector<int> user_page_counts_;  // индекс вектора [user_id] -> кол-во страниц
-  // vector<int> sorted_users_;   // user_id внутри; отсортированы по убыванию количества страниц
-  // vector<int> user_positions_; // позиции в векторе sorted_users_
+  set<Reader> readers;
+  vector<bool> registered_users;
+  vector<myIt> reader_access;
 
   int GetUserCount() const {    // O(1)
     return readers.size(); 
   }
-
-  set<Reader> readers;
-  // set<Reader> readers;
-  vector<bool> registered_users;
-  vector<myIt> reader_access;
   
   void AddUser(int user_id, int pages = 0) {
     // cerr << "Adding user " << user_id << "..." << endl;
     try
     {
-      pair<myIt, bool> add = readers.insert({user_id, pages});
+      pair<myIt, bool> add = readers.insert({pages, user_id});
       // cerr << "User added" << endl;
       registered_users[user_id] = true;
       // cerr << "User registered" << endl;
@@ -120,50 +103,42 @@ private:
     // cerr << "target caught" << endl;
     readers.erase(target);
     // cerr << "target erased" << endl;
-    target = readers.insert({user_id, pages}).first;
+    target = readers.insert({pages, user_id}).first;
     // cerr << "target updated" << endl;
   }
-
-  // void SwapUsers(int lhs_position, int rhs_position) {
-  //   const int lhs_id = sorted_users_[lhs_position];
-  //   const int rhs_id = sorted_users_[rhs_position];
-  //   swap(sorted_users_[lhs_position], sorted_users_[rhs_position]); // O(1)
-  //   swap(user_positions_[lhs_id], user_positions_[rhs_id]);         // O(1)
-  // }
 };
 
 
-// void TestReadingManager() {
-//   // Coursera Test #1
-//   ReadingManager rm;
-//   ASSERT_EQUAL(rm.Cheer(5), 0);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Read(1, 10);
-//   cerr << __LINE__ << " done" << endl;
-//   ASSERT_EQUAL(rm.Cheer(1), 1);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Read(2, 5);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Read(3, 7);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Dump();
-//   ASSERT_EQUAL(rm.Cheer(2), 0);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Dump();
-//   ASSERT_EQUAL(rm.Cheer(3), 0.5);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Read(3, 10);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Dump();
-//   ASSERT_EQUAL(rm.Cheer(3), 0.5);
-//   cerr << __LINE__ << " done" << endl;
-//   rm.Read(3, 11);
-//   cerr << __LINE__ << " done" << endl;
-//   ASSERT_EQUAL(rm.Cheer(3), 1);
-//   cerr << __LINE__ << " done" << endl;
-//   ASSERT_EQUAL(rm.Cheer(1), 0.5);
-//   cerr << __LINE__ << " done" << endl;
-// }
+void TestReadingManager() {
+  // Coursera Test #1
+  ReadingManager rm;
+  ASSERT_EQUAL(rm.Cheer(5), 0);
+  cerr << __LINE__ << " done" << endl;
+  rm.Read(1, 10);
+  cerr << __LINE__ << " done" << endl;
+  ASSERT_EQUAL(rm.Cheer(1), 1);
+  cerr << __LINE__ << " done" << endl;
+  rm.Read(2, 5);
+  cerr << __LINE__ << " done" << endl;
+  rm.Read(3, 7);
+  cerr << __LINE__ << " done" << endl;
+  ASSERT_EQUAL(rm.Cheer(2), 0);
+  cerr << __LINE__ << " done" << endl;
+  ASSERT_EQUAL(rm.Cheer(3), 0.5);
+  cerr << __LINE__ << " done" << endl;
+  rm.Read(3, 10);
+  ASSERT_EQUAL(rm.Cheer(1), 0.5);
+  cerr << __LINE__ << " done" << endl;
+  rm.Dump();
+  ASSERT_EQUAL(rm.Cheer(3), 0.5);
+  cerr << __LINE__ << " done" << endl;
+  rm.Read(3, 11);
+  cerr << __LINE__ << " done" << endl;
+  ASSERT_EQUAL(rm.Cheer(3), 1);
+  cerr << __LINE__ << " done" << endl;
+  ASSERT_EQUAL(rm.Cheer(1), 0.5);
+  cerr << __LINE__ << " done" << endl;
+}
 
 int main() {
   // Для ускорения чтения данных отключается синхронизация
@@ -173,29 +148,29 @@ int main() {
   cin.tie(nullptr);
   // TODO: ^^^ проверь, что тут происходит ^^^
 
-  // TestRunner tr;
+  TestRunner tr;
 
-  // RUN_TEST(tr, TestReadingManager);
+  RUN_TEST(tr, TestReadingManager);
 
-  ReadingManager manager;
+  // ReadingManager manager;
 
-  int query_count;
-  cin >> query_count;
+  // int query_count;
+  // cin >> query_count;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-    int user_id;
-    cin >> user_id;
+  // for (int query_id = 0; query_id < query_count; ++query_id) {
+  //   string query_type;
+  //   cin >> query_type;
+  //   int user_id;
+  //   cin >> user_id;
 
-    if (query_type == "READ") {
-      int page_count;
-      cin >> page_count;
-      manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
-    }
-  }
+  //   if (query_type == "READ") {
+  //     int page_count;
+  //     cin >> page_count;
+  //     manager.Read(user_id, page_count);
+  //   } else if (query_type == "CHEER") {
+  //     cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+  //   }
+  // }
 
   return 0;
 }
