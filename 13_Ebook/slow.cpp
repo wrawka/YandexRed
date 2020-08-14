655a2dfc5a6c8ea56f8a4b137127ef3cd9cca7ea
wrawka <wrawka@gmail.com>
2020-08-14 16:58:29 +0300

    Complexity solved

--- 13_Ebook/slow.cpp
+++ 13_Ebook/slow.cpp
@@ -16,14 +16,19 @@ void PrintVector(vector<T> source, string hint) {
   cout << endl;
 }
 
+/*Q — количество запросов
+* U — максимальный номер пользователя
+* P — максимальное значение номера страницы
+*/
+
 class ReadingManager {
 public:
-  ReadingManager()
+  ReadingManager() //O(U)
       : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
         sorted_users_(),
         user_positions_(MAX_USER_COUNT_ + 1, -1) {}
 
-  void Read(int user_id, int page_count) {
+  void Read(int user_id, int page_count) { // O(Q^2)
     if (user_page_counts_[user_id] == 0) {
       AddUser(user_id);
     }
@@ -34,7 +39,7 @@ public:
     }
   }
 
-  double Cheer(int user_id) const {
+  double Cheer(int user_id) const { // O(Q^2)
     if (user_page_counts_[user_id] == 0) {
       return 0;
     }
@@ -111,7 +116,7 @@ void TestReadingManager() {
   rm.Dump();
 }
 
-int main() {
+int main() { // O(U + Q^2)
   // Для ускорения чтения данных отключается синхронизация
   // cin и cout с stdio,
   // а также выполняется отвязка cin от cout
@@ -123,25 +128,25 @@ int main() {
 
   RUN_TEST(tr, TestReadingManager);
 
-  // ReadingManager manager;
+  ReadingManager manager;
 
-  // int query_count;
-  // cin >> query_count;
+  int query_count;
+  cin >> query_count;
 
-  // for (int query_id = 0; query_id < query_count; ++query_id) {
-  //   string query_type;
-  //   cin >> query_type;
-  //   int user_id;
-  //   cin >> user_id;
+  for (int query_id = 0; query_id < query_count; ++query_id) {
+    string query_type;
+    cin >> query_type;
+    int user_id;
+    cin >> user_id;
 
-  //   if (query_type == "READ") {
-  //     int page_count;
-  //     cin >> page_count;
-  //     manager.Read(user_id, page_count);
-  //   } else if (query_type == "CHEER") {
-  //     cout << setprecision(6) << manager.Cheer(user_id) << "\n";
-  //   }
-  // }
+    if (query_type == "READ") {
+      int page_count;
+      cin >> page_count;
+      manager.Read(user_id, page_count);
+    } else if (query_type == "CHEER") {
+      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+    }
+  }
 
   return 0;
 }
