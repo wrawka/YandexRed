08559dd7380926924e9099142438872f70b79822
wrawka <wrawka@gmail.com>
2020-10-22 17:11:31 +0300

    Not working but promising ^^

---
+++ 24_ServerStats/stats.cpp
@@ -0,0 +1,38 @@
+#include "stats.h"
+
+#include <iostream>
+
+void Stats::AddMethod(string_view method) {
+  if (method_stats.find(method) != method_stats.end()) {
+  	method_stats.at(method)++;
+	} else {
+		method_stats.at("UNKNOWN")++;
+	}
+}
+
+void Stats::AddUri(string_view uri) {
+  if (uri_stats.find(uri) != uri_stats.end()) {
+  	uri_stats.at(uri)++;
+	} else {
+		uri_stats.at("unknown")++;
+	}
+}
+
+const map<string_view, int>& Stats::GetMethodStats() const {
+  return method_stats;
+}
+
+const map<string_view, int>& Stats::GetUriStats() const {
+    return uri_stats;
+}
+
+HttpRequest ParseRequest(string_view line) {
+  HttpRequest return_value;
+    for (auto out : {return_value.method, return_value.uri, return_value.protocol}) {
+        auto c_count = line.find_first_of(" ");
+        out = string_view(line.data(), c_count);
+        line.remove_prefix(c_count);
+    }
+		std::cout << return_value.method << "/" << return_value.uri << "/" << return_value.protocol << std::endl;
+    return return_value;
+}
