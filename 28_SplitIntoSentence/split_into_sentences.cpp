72e460f6a91384d6e1d9030aa6b8bb76c2dd6ab9
wrawka <wrawka@gmail.com>
2020-11-26 14:07:36 +0300

    working version

--- 28_SplitIntoSentence/split_into_sentences.cpp
+++ 28_SplitIntoSentence/split_into_sentences.cpp
@@ -1,8 +1,16 @@
 #include "test_runner.h"
+// #include "logging.h"
 
 #include <vector>
 
 using namespace std;
+// Максимальное количество токенов — 10^6
+// Время выполнения одного вызова функции ограничено 1 секундой
+
+/*
+auto isEnd {
+  [](auto t){return true;}
+};*/
 
 // Объявляем Sentence<Token> для произвольного типа Token
 // синонимом vector<Token>.
@@ -16,8 +24,23 @@ using Sentence = vector<Token>;
 template <typename Token>
 vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
   // Напишите реализацию функции, не копируя объекты типа Token
+  vector<Sentence<Token>> result;
+  Sentence<Token> new_sentence;
+  for (auto it = tokens.begin(); it != tokens.end(); it++) {
+    new_sentence.push_back(move(*it));
+    if  ( it -> IsEndSentencePunctuation() && 
+        ( next(it) == tokens.end() || !(next(it) -> IsEndSentencePunctuation()) )
+        )
+    {
+      // PrintRange(new_sentence.begin(), new_sentence.end());
+      result.push_back(move(new_sentence));
+    }
+  }
+  if (!new_sentence.empty()) {
+    result.push_back(move(new_sentence));
+  }
+  return result;
 }
-
 
 struct TestToken {
   string data;
@@ -31,7 +54,30 @@ struct TestToken {
   }
 };
 
+// некопируемая версия Token
+struct NCToken {
+  string data;
+  bool is_end_sentence_punctuation = false;
+
+  NCToken(const NCToken&) = delete;
+  NCToken& operator=(const NCToken&) = delete;
+
+  NCToken(NCToken&&) = default;
+  NCToken& operator=(NCToken&&) = default;
+
+  bool IsEndSentencePunctuation() const {
+    return is_end_sentence_punctuation;
+  }
+  bool operator==(const NCToken& other) const {
+    return data == other.data && is_end_sentence_punctuation == other.is_end_sentence_punctuation;
+  }
+};
+
 ostream& operator<<(ostream& stream, const TestToken& token) {
+  return stream << token.data;
+}
+
+ostream& operator<<(ostream& stream, const NCToken& token) {
   return stream << token.data;
 }
 
