8d57cb8c1115dccb6469c8fabfe663e7e937d625
wrawka <wrawka@gmail.com>
2020-12-30 16:11:44 +0300

    added commenst

--- 06_UNIQ_ID/uniq_id.cpp
+++ 06_UNIQ_ID/uniq_id.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 using namespace std;
 
+// макрос "UNIQ_ID" передаёт __LINE__ как параметр макросу "f"
 #define UNIQ_ID f(__LINE__)
+// макрос "f" разворачивает __LINE__ непосредственно в номер строки и передаёт его как параметр макросу "g"
 #define f(a) g(a)
+// макрос "g" склеивает ID с номером строки
 #define g(a) ID##a
 
 int main() {
