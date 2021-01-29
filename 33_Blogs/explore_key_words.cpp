0ba77a16fd934ef80a3bd83d10a6785df57bb2ec
wrawka <wrawka@gmail.com>
2021-01-29 15:19:32 +0300

    This shit is working somehow

--- 33_Blogs/explore_key_words.cpp
+++ 33_Blogs/explore_key_words.cpp
@@ -1,6 +1,6 @@
 #include "test_runner.h"
-#include "profile.h"
-#include "logging.h"
+// #include "profile.h"
+// #include "logging.h"
 
 #include <map>
 #include <string>
@@ -9,6 +9,8 @@
 #include <iterator>
 using namespace std;
 
+const int CHUNK_SIZE = 1'000;
+/*
 // Paginator
 template <typename Iterator>
 class IteratorRange {
@@ -49,7 +51,7 @@ auto Paginate(C& c, size_t page_size) {
   return Paginator(c.begin(), c.end(), page_size);
 }
 // end of Paginator
-
+*/
 struct Stats {
   map<string, int> word_frequences;
 
@@ -60,13 +62,15 @@ struct Stats {
   }
 };
 
+void ClearWhitespace(string_view& sw) {
+  sw.remove_prefix(min(sw.find_first_not_of(' '), sw.size()));
+}
+
 Stats ExploreLine(const set<string>& key_words, const string& line) {
   string_view line_view(line);
-  line_view.remove_prefix(
-    min( line_view.find_first_not_of(' '), line_view.size() )
-  ); // drop leading whitespaces if any
   Stats result;
   while(true) {
+    ClearWhitespace(line_view); // drop leading whitespaces if any
     size_t space = line_view.find(' '); // looking for the next whitespace
     string word(line_view.substr(0, space));
     if (key_words.find(word) != key_words.end()) {
@@ -78,7 +82,7 @@ Stats ExploreLine(const set<string>& key_words, const string& line) {
       line_view.remove_prefix(space + 1);
     }
   }
-  PrintMap(result.word_frequences);
+  // PrintMap(result.word_frequences);
   return result;
 }
 
@@ -92,13 +96,22 @@ Stats ExploreKeyWordsSingleThread(const set<string>& key_words, istream& input) 
 
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
   //TODO: split the input stream, NOT the dictionary
-  const size_t CHUNK_SIZE = key_words.size() / 2;
   vector<future<Stats>> futures;
   Stats result;
-  for (auto chunk : Paginate(key_words, CHUNK_SIZE)) {
-    set<string> c_set{begin(chunk), end(chunk)};
-    // PrintRange(c_set.begin(), c_set.end());
-    futures.push_back(async(ExploreKeyWordsSingleThread, c_set, ref(input)));
+  vector<string> what;
+  while (input) {
+    string temp_s;
+    for (auto i = 0; (i < CHUNK_SIZE) && (input); i++) {
+      string temp;
+      getline(input, temp);
+      temp_s += " " + temp;
+      // PRINT_VALUE(i);
+    }
+    what.push_back(temp_s);
+  }
+  // PrintRange(begin(what), end(what));
+  for (auto& out : what) {
+    futures.push_back(async(ExploreLine, ref(key_words), ref(out)));
   }
   for (auto& f : futures) {
     result += f.get();
@@ -110,11 +123,11 @@ void TestBasic() {
   const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
 
   stringstream ss;
-  ss << "this new yangle service really rocks\n";
+  ss << "     this new yangle service really rocks\n";
   ss << "It sucks when yangle isn't available\n";
-  ss << "10 reasons why yangle is the best IT company\n";
+  ss << "10 reasons why yangle is         the best IT company\n";
   ss << "yangle rocks others suck\n";
-  ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+  ss << "Goondex really sucks, but yangle    rocks. Use yangle\n";
 
   const auto stats = ExploreKeyWords(key_words, ss);
   const map<string, int> expected = {
